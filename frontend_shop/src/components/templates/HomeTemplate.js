a17c4c108f99e477dc7675b1bf22c02282eba592
cosmostart <86516619+cosmostart@users.noreply.github.com>
2023-07-20 20:13:15 +1000

    Add files via upload

--- frontend_shop/src/components/templates/HomeTemplate.js
+++ frontend_shop/src/components/templates/HomeTemplate.js
@@ -1,1 +1,15 @@
+import CategoryGrid from '../organisms/CategoryGrid';
+import ProductGrid from '../organisms/ProductGrid';
 
+function HomeTemplate(props) {
+    return (
+        <main className="mt-4">
+        <div className="container">
+            <CategoryGrid categories={props?.['homeTemplate']?.[0]}/>
+            <ProductGrid products={props?.['homeTemplate']?.[1]}/>
+        </div>
+    </main>
+    )
+}
+
+export default HomeTemplate;
